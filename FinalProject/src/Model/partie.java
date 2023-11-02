87434745cef9569d3dbd8ad88cd87efafa80d707
Jasroj <basurarojas224@gmail.com>
2023-11-02 15:46:33 -0600

    Class partie

---
+++ FinalProject/src/Model/partie.java
@@ -0,0 +1,73 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author JRS
+ */
+public class partie {
+
+    private int id;
+    private String name;
+    private int id_candidate;
+
+    public partie(int id, String name, int id_candidate) {
+        this.id = id;
+        this.name = name;
+        this.id_candidate = id_candidate;
+    }
+
+    public partie(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public partie() {
+
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the id_candidate
+     */
+    public int getId_candidate() {
+        return id_candidate;
+    }
+
+    /**
+     * @param id_candidate the id_candidate to set
+     */
+    public void setId_candidate(int id_candidate) {
+        this.id_candidate = id_candidate;
+    }
+}
